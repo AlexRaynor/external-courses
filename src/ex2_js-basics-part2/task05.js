60eb9cd5f7017effce146314797a2daac030c8cf
AlexRaynor <36274821+AlexRaynor@users.noreply.github.com>
2018-11-25 15:10:27 +0300

    Update task 5

--- src/ex2_js-basics-part2/task05.js
+++ src/ex2_js-basics-part2/task05.js
@@ -1,18 +1,10 @@
-function maxElement() {
-    var arr=[];
-    var max;
-       let items = prompt("Перечислите через запятую элементы", "");
-          if ((typeof(items)) === "string" && (typeof(items)) !== null && items !== ""){
-                    console.log('ввод верный');
-                    arr = items.split(",");
-              
-                } else {
-            alert ("Введите корректные данные");
-              i--;
-              }
-  max = Math.max.apply(null, arr);
-  
-  console.log("Максимальный элемент: " + max);
+function getMax(arr){
+    var max = arr[0];
+    for(var i = 1; i< arr.length;i++){
+        if (arr[i]>max){
+            max = arr[i];
+        }
     }
-  
-maxElement();
+    console.log("Максимальное значение: " + max)
+}
+getMax([1,2,3,9,35,4]);
