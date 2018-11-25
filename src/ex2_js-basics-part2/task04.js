2c9146872897be36b8927c4746a11c5077e22e17
AlexRaynor <36274821+AlexRaynor@users.noreply.github.com>
2018-11-25 15:09:43 +0300

    Update task 4

--- src/ex2_js-basics-part2/task04.js
+++ src/ex2_js-basics-part2/task04.js
@@ -1,24 +1,11 @@
-function isTrue() {
-    var arr=[];
-    var i;
-       let items = prompt("Перечислите через запятую элементы", "");
-          if ((typeof(items)) === "string" && (typeof(items)) !== null && items !== ""){
-                    console.log('ввод верный');
-                    arr = items.split(",");
-              
-                } else {
-            alert ("Введите корректные данные");
-              i--;
-              }
-  
-    for (i = 0; i < arr.length; i++) {
-  if (arr[i] === arr[i-1]) {
-    console.log(true);
-  }
-  else{
-    console.log(false);
-  }
-  
-      }
-  }
-  isTrue();
+function similarCheck(arr){
+    var result = true;
+    for(var i =1; i<arr.length;i++){
+        if(arr[i]!==arr[i-1]){
+            result = false;
+            break;
+        }
+    }
+    console.log(result)
+}
+similarCheck([1,1,1,2]);
